82c4c981f8b9586bc914b96250885e73d98de022
ue143014 <anupagrawal81@gmail.com>
2019-11-27 11:26:21 +0530

    Add more Features like display total number of elements

--- parking.rb
+++ parking.rb
@@ -51,14 +51,34 @@ class ParkingLot
         end
     end
 
+    def removeSpace
+        @actualPositions.each do |ele|
+            @parking_lot_dimensions[ele[0]][ele[1]] = 0
+        end
+    end
+
+    def checkParkedVehicle(value)
+        return @actualPositions.all? {|ele| @parking_lot_dimensions[ele[0]][ele[1]] == value} ? true : false
+    end
+
+    def displayParkingLot
+        @parking_lot_dimensions.each do |rows|
+            puts rows.map { |columns| columns.to_s }.join("\t")
+        end
+    end
+
+
 end
 
 class Vehicle
-    @@Number_of_Vehicles = 0
+    @@Number_of_Vehicles_parked = 0
+    @@Number_of_Vehicles_requested = 0
+    @@Number_of_Vehicles_removed = 0
+
     attr_accessor :required_space, :vehicle_type
 
     def initialize(spot_needed,vehicle_type)
-        @@Number_of_Vehicles += 1
+        @@Number_of_Vehicles_requested += 1
         @required_space = spot_needed
         @vehicle_type = vehicle_type
     end
@@ -71,9 +91,27 @@ class Vehicle
             if(parkingObject.checkAvailability)
                 puts "Congrats !! Space is also available. Let's Park your vehicle -"
                 parkingObject.allocateSpace(@vehicle_type)
-                puts "Your vehicle successfully parked"
+                puts "Your vehicle has successfully parked"
+                @@Number_of_Vehicles_parked += 1
             else
                 puts "Sorry !! Given location is not available for parking !!"
+            end
+        else
+            puts "Sorry !! Given location is not Valid."
+        end
+    end
+
+    def removeVehicleMechaism(parkingObject)
+        puts "Let's check the validity of your location - "
+        if(parkingObject.validateLocation)
+            puts "Congrats !! Your location is successfully Validated !!\nLet's Check Vehicle Parked or not? - "
+            if(parkingObject.checkAvailability)
+                puts "Congrats !! Mentioned vehicle is parked at given location. Let's remove your vehicle -"
+                parkingObject.allocateSpace(@vehicle_type)
+                puts "Your vehicle has successfully removed."
+               @@Number_of_Vehicles_removed -= 1
+            else
+                puts "Sorry !! Mentioned vehicle is not parked at given location !!"
             end
         else
             puts "Sorry !! Given location is not Valid."
@@ -128,17 +166,26 @@ while(user_yes_no == "Y") do
 
     userSelection = gets.chomp.to_i
 
+    if !([4,5,6,7,8].include? userSelection)
+        puts "Please Specify Indexes/Location"
+        indexes = gets.chomp.split(" ").map {|x| x.to_i}
+        parkingLotObject.givenPositions = indexes
+    end
+    
     case userSelection
     when 1
         vehicle = Bicycle.new(1,"Bicycle")
+        vehicle.parkedMechanism(parkingLotObject)
     when 2
         vehicle = Bike.new(2,"Bike")
+        vehicle.parkedMechanism(parkingLotObject)
     when 3
         vehicle = Car.new(3,"Car")
+        vehicle.parkedMechanism(parkingLotObject)
     when 4
-
+        parkingLotObject.displayParkingLot
     when 5
-
+        parkingLotObject.displayParkingLot
     when 6
         puts vehicle.bicycleParked
     when 7
@@ -146,19 +193,22 @@ while(user_yes_no == "Y") do
     when 8
         puts vehicle.carsParked
     when 9
-        # needs tp done
+        vehicle = Bicycle.new(1,"Bicycle")
+        vehicle.removeVehicleMechaism(parking_lot_dimensions)
     when 10
-        # NEEDS TO be done
+        vehicle = Bike.new(2,"Bike")
+        vehicle.removeVehicleMechaism(parking_lot_dimensions)
     when 11
-        # needs to be done
+        vehicle = Car.new(4,"Bicycle")
+        vehicle.removeVehicleMechaism(parking_lot_dimensions)
     else
         puts "Please select any option from the above only !!"
+    end
+    # indexes = gets.chomp.split(" ").map {|x| x.to_i}
+    # parkingLotObject.givenPositions = indexes
 
-    indexes = gets.chomp.split(" ").map {|x| x.to_i}
-    parkingLotObject.givenPositions = indexes
-
-    carObj = Car.new(2,"Car")
-    carObj.parkedMechanism(parkingLotObject)
+    # carObj = Car.new(2,"Car")
+    # carObj.parkedMechanism(parkingLotObject)
 
     puts "Want to perform more operation ? [y/n]"
     user_yes_no = gets.chomp.upcase
